b6920fca25392f2e608b21f3c4d9eaa2996a2d33
Nevile Pitters <nevile_pitters@mednax.com>
2023-02-09 13:02:45 -0500

    Revert "Added unit test for valid and invalid target id"
    This reverts commit 21b1e577555631d211fb895a38984aa73a920667.

--- app/transformer.py
+++ app/transformer.py
@@ -1,13 +1,3 @@
-from app.models.national_track_and_field_database import TrackData
 
-
-class MissingRequiredValueException(Exception):
+def transform(raw_data):
     pass
-
-
-def transform(raw_data: TrackData):
-    if not raw_data:
-        raise MissingRequiredValueException("Missing or required field 'id'")
-    if not raw_data["id"]:
-        raise MissingRequiredValueException("Missing or required field 'id'")
-    return raw_data
--- tests/test_transformer.py
+++ tests/test_transformer.py
@@ -1,27 +1,7 @@
-import pytest
-import json
-from app.transformer import transform, MissingRequiredValueException
+from app.transformer import transform
 
-def test_transform_with_valid_id_field_returns():
-    with open("tests/test_data/valid_ntfd_data.json", encoding="utf-8") as f:
-        data = json.load(f)
 
-    result = transform(data)
+def test_transform_returns_something():
+    result = transform({"id": 1})
 
     assert result is not None
-
-def test_transform_with_invalid_id_field_returns_exception():
-    with open("tests/test_data/invalid_id_field.json", encoding="utf-8") as f:
-        data = json.load(f)
-
-    with pytest.raises(MissingRequiredValueException) as err:
-        transform(data)
-        assert "Missing or invalid required field 'id'" in str(err.value)
-
-def test_transform_with_missing_id_field_returns_exception():
-    with open("tests/test_data/missing_id_field.json", encoding="utf-8") as f:
-        data = json.load(f)
-
-    with pytest.raises(MissingRequiredValueException) as err:
-        transform(data)
-        assert "Missing or required field 'id'" in str(err.value)
