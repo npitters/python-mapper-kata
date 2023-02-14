b06137c673f723fa8d213bb35ed3ba9bfddbccb7
Nevile Pitters <nevile_pitters@mednax.com>
2023-02-14 08:40:26 -0500

    Removed test data json files

--- tests/models/test_ntf_db_model.py
+++ tests/models/test_ntf_db_model.py
@@ -2,8 +2,15 @@ import json
 from app.models.ntf_db_model import TrackAndFieldDb
 
 def test_from_json_with_valid_schema():
-    with open("tests/models/test_data/valid_fields.json", encoding="utf-8") as f:
-        data = json.load(f)
+    data = {
+        "id": 101,
+        "name": "Jumper Smith",
+        "class": "Junior",
+        "eventClassification": "B",
+        "eventTypeId": 2,
+        "eventId": 2003,
+        "marks": "13ft 1in|9ft 5in|14ft 0in|13ft 5in|14ft 5in"
+    }
 
     result = TrackAndFieldDb.from_json(data)
 
--- tests/test_transformer.py
+++ tests/test_transformer.py
@@ -1,31 +1,43 @@
 import pytest
 import json
-from app.transformer import transform, MissingRequiredFieldException
+from app.transformer import transform
 
 
-def test_transform_with_valid_id_field():
-    with open("tests/models/test_data/valid_fields.json", encoding="utf-8") as f:
-        data = json.load(f)
+
+@pytest.fixture()
+def transform_required_fields():
+    with patch("transformer.MissingRequiredFieldException") as mock:
+        yield mock
+def test_transform_with_valid_id_maps_id():
+    data = {
+        "id": 101,
+        "name": "Jumper Smith",
+        "class": "Junior",
+        "eventClassification": "B",
+        "eventTypeId": 2,
+        "eventId": 2003,
+        "marks": "13ft 1in|9ft 5in|14ft 0in|13ft 5in|14ft 5in"
+    }
 
     result = transform(data)
 
-    assert result is not None
+    assert result["id"] == 101
 
 def test_transform_with_invalid_id_field_returns_exception():
-    with open("tests/models/test_data/invalid_fields.json", encoding="utf-8") as f:
-        data = json.load(f)
+    data = {"id": None}
 
-    with pytest.raises(MissingRequiredFieldException) as err:
+    with pytest.raises(Exception) as err:
         transform(data)
-    assert "Missing required field" in str(err.value)
+
+    assert err.value.args[0] == "Missing required field"
 
 def test_transform_with_missing_id_field_returns_exception():
-    with open("tests/models/test_data/missing_fields.json", encoding="utf-8") as f:
-        data = json.load(f)
+    data = {}
 
-    with pytest.raises(MissingRequiredFieldException) as err:
+    with pytest.raises (Exception) as err:
         transform(data)
-    assert "Missing required field" in str(err.value)
+
+    assert err.value.args[0] == "Missing required field"
 
 def test_transform_with_valid_name_maps_lastname_firstname():
     data = {
